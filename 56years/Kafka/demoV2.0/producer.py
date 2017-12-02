09e301816f5f964c70eafab529601730ef62256c
Ryan <rk635@cornell.edu>
2017-12-01 21:46:55 -0500

    renamed file

--- 56years/Kafka/demoV2.0/producer.py
+++ 56years/Kafka/demoV2.0/producer.py
@@ -1,31 +1,30 @@
-#kafka-python
-#python-twitter
-#tweepy
-
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-from kafka import SimpleProducer, KafkaClient
 
+access_token = "936766436772663297-lDZ1AyP3z6NiZ1L0qQLQdo5PQXW6VZR"
+access_token_secret = "NY1x4ZIdTjBDgfMAIgknz1urSPE3AZK2tDPEwyXIl3ovS"
+consumer_key = "7Dzvyp5IKeB0dgj9wdcBEP2Fi"
+consumer_secret = "eTxZwEydglZeQIZguMiYcaQokRzNA04MG1mOY2uwEwMEZhxNd9"
 topic = ""
-search = ""
-access_token = ""
-access_token_secret = ""
-consumer_key = ""
-consumer_secret = ""
 
-class out(StreamListener):
-    def onVerify(self, data):
-        producer.send_messages(topic, data.encode('utf-8'))
-        print(data)
+
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        #producer.send_messages(topic, data.encode('utf-8'))
+        print data
         return True
-    def error(self, status):
-        print(status)
 
-kafka = KafkaClient("localhost:9092")
-producer = SimpleProducer(kafka)
-listen = StdOutListener()
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-stream = Stream(auth, listen)
-stream.filter(track=search)
+    def on_error(self, status):
+        print status
+
+
+if __name__ == '__main__':
+    #kafka = KafkaClient("localhost:9092")
+    #producer = SimpleProducer(kafka)
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+    stream.filter(track=['pokemon', 'kafka', 'zelda'])
